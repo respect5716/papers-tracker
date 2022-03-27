0739a3323f6ac77edbd8210f2067896504f0fa21
Yoon <respect5716@gmail.com>
2022-03-27 22:42:20 +0900

    Update method

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # papers-tracker
+
+
+## Run
+```
+export EMAIL_ADDRESS=my_email_address
+export EMAIL_PASSWORD=my_email_password
+
+pip install -r requirements.txt
+python main.py
+```
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import re
 import requests
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 
 import arxiv
@@ -8,6 +9,13 @@ import pandas as pd
 
 import smtplib
 from email.mime.text import MIMEText
+
+
+def get_target_date(delta=-3):
+    now = datetime.utcnow()
+    date = now + timedelta(days=delta)
+    date = date.strftime('%Y%m%d')
+    return date
 
 
 def read_seeds(fpath):
@@ -18,49 +26,35 @@ def read_seeds(fpath):
     seeds = pd.DataFrame({'title': title_list, 'arxiv_id': id_list})
     return seeds
 
-def url_to_soup(url):
-    res = requests.get(url)
-    html = res.text
-    soup = BeautifulSoup(html, 'html.parser')
-    return soup
 
-def get_cites_url(arxiv_id, start=0):
-    soup = url_to_soup(f'https://scholar.google.com/scholar_lookup?arxiv_id={arxiv_id}')
-    hrefs = [a.get('href') for a in soup.select('a')]
-    hrefs = [h for h in hrefs if 'scholar?cites' in h]
-    if hrefs:
-        cites = 'https://scholar.google.com' + hrefs[0] + f'&start={start}&scisbd=1'
-    else:
-        cites = None
-    return cites
-
-
-def get_citing_papers(arxiv_id, age=3):
-    res = pd.DataFrame(columns=['title', 'url', 'age'])
-
-    start = 0
-    while True:
-        cites_url = get_cites_url(arxiv_id, start=start)
-        if not cites_url: break
-            
-        soup = url_to_soup(cites_url)
-        papers = soup.select('h3.gs_rt')
-
-        ages = [i.text for i in soup.select('span.gs_age')]
-        ages = [''.join(filter(str.isdigit, a)) for a in ages]
-        titles = [i.a.text for i in papers]
-        urls = [i.a.get('href') for i in papers]
-
-        _res = pd.DataFrame({'title': titles, 'url': urls, 'age': ages})
-        _res = _res.loc[_res['age'] == str(age)]
-
-        res = pd.concat([res, _res], ignore_index=True)
-        start += 10
+def find_followers_id_list(arxiv_id, yymm, limit=100):
+    url = f'https://api.semanticscholar.org/v1/paper/arXiv:{arxiv_id}'
+    res = requests.get(url).json()
+    if res.get('error'):
+        return []
+    followers = pd.DataFrame(res['citations'])
+    if len(followers) == 0:
+        return []
         
-        if len(_res) == 0:
-            break
+    followers = followers.dropna(subset=['arxivId'])
+    followers = followers.loc[followers['arxivId'].apply(lambda x: x.split('.')[0] == yymm)]
+    followers = followers.sort_values('arxivId', ascending=False)
+    id_list = followers['arxivId'].iloc[:limit].tolist()
+    return id_list
 
-    return res
+
+def search_followers(fid_list):
+    if not fid_list:
+        return pd.DataFrame(columns=['title', 'url', 'date'])
+    
+    res = arxiv.Search(id_list=fid_list).results()
+    followers = []
+    for r in res:
+        followers.append({'title': r.title, 'url': r.entry_id, 'date': r.published.strftime('%Y%m%d')})
+    followers = pd.DataFrame(followers)
+    return followers
+
+
 
 
 def write_doc(seeds, citations):
@@ -80,13 +74,16 @@ def write_doc(seeds, citations):
 
 
 def main():
+    date = get_target_date()
     seeds = read_seeds('papers.txt')
 
     citations = {}
     for idx, row in seeds.iterrows():
         arxiv_id = row['arxiv_id']
-        citations[arxiv_id] = get_citing_papers(arxiv_id)
-    
+        fid_list = find_followers_id_list(arxiv_id, date[2:-2])
+        followers = search_followers(fid_list)
+        followers = followers.loc[followers['date'] == date]
+        citations[arxiv_id] = followers
     doc = write_doc(seeds, citations)
 
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
